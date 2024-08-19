77dd0e1db6fe48a3183043637577a3f33300f703
Rafal Labunski <rafallabunski@gmail.com>
2024-08-19 16:20:46 +0200

    Add playerMove variable and display combined message with computerMove

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+// Deklaracja zmiennej dla ruchu komputera
+let computerMove = 'kamień';
+
+// Deklaracja zmiennej dla ruchu gracza
+let playerMove = 'papier';
+
+// Wyświetlenie komunikatu z użyciem obu zmiennych
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
